b187844f5dcc6146dc262938e447e48e419557e4
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-03-04 20:35:47 +0100

    user router connected to server, additional edits made in route file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const userRouter = require("./routes/songRoutes");
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +9,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("User API is live!");
 });
+
+app.use("/users", userRouter)
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
