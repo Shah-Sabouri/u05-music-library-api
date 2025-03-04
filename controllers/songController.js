f57b9075ec04ba514e5d47c5ea9b151d9a482aeb
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-03-04 19:43:12 +0100

    coding in controller started

---
+++ controllers/songController.js
@@ -0,0 +1,27 @@
+let songs = require("../models/songModel");
+
+// HÄMTA BEFINTLIGA LÅTAR
+const getAllSongs = (req, res) => {
+    
+};
+
+// HÄMTA LÅT VIA ID
+const getSongById = (req, res) => {
+
+};
+
+// SKAPA NY LÅT
+const createSong = (req, res) => {
+
+};
+
+// UPPDATERA LÅT
+const updateSong = (req, res) => {
+
+};
+
+// RADERA LÅT
+const deleteSong = (req, res) => {
+
+};
+
