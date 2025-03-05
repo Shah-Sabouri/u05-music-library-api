5c6a5253325f4334cbd201480bfba68d1d3861b9
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-03-05 19:35:42 +0100

    songModel.js updated from dummy data to a proper songModel.js

--- models/songModel.js
+++ models/songModel.js
@@ -1,7 +1,10 @@
-const songs = [
-    { id: "1", title: "Picture Me Rollin", artist: "2pac" },
-    { id: "2", title: "Time to Get it Together", artist: "Marvin Gaye" },
-    { id: "3", title: "More Bounce To the Ounce", artist: "Zapp & Roger" },
-];
+const mongoose = require("mongoose");
 
-module.exports = songs;
+const songSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    artist: { type: String, required: true },
+    genre: { type: String },
+    rating: { type: Number, min: 1, max: 5 }
+});
+
+module.exports = mongoose.model("Song", songSchema);
