e6e145afaf5405c85c43a8ea1a5690122109ae4e
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-03-05 19:44:42 +0100

    old dummy data in songModel.js commented out

--- models/songModel.js
+++ models/songModel.js
@@ -1,3 +1,11 @@
+/* const songs = [
+    { id: "1", title: "Picture Me Rollin", artist: "2pac" },
+    { id: "2", title: "Time to Get it Together", artist: "Marvin Gaye" },
+    { id: "3", title: "More Bounce To the Ounce", artist: "Zapp & Roger" },
+];
+
+module.exports = songs; */
+
 const mongoose = require("mongoose");
 
 const songSchema = new mongoose.Schema({
