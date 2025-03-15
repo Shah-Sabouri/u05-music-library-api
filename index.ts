f2e8d38ce92452a3255503448732c6441e33cdeb
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-03-15 02:01:09 +0100

    Correct port handling for deployment on Render

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import songRoutes from "./routes/songRoutes";
 dotenv.config();
 
 const app: Application = express();
-const PORT = process.env.PORT || 3000;
+const PORT: number = parseInt(process.env.PORT || "3000", 10);
 
 app.get("/", (req: Request, res: Response): void => {
     res.send("Music Library API is live!");
@@ -19,6 +19,6 @@ mongoose
     .connect(process.env.MONGO_DB as string)
     .then(() => {
         console.log("Connected to MongoDB");
-        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+        app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
     })
     .catch((error) => console.log("Database connection error:", error));
