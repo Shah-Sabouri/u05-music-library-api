5876a3edb6d53e061630d553c06e5ebed398d5c5
Shahryar <shahryar.sabouri.1301@gmail.com>
2025-04-14 19:29:25 +0200

    Fixed CORS to allow frontend

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import express, { Application, Request, Response } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 import songRoutes from "./routes/songRoutes";
 
 dotenv.config();
 
 const app: Application = express();
 const PORT: number = parseInt(process.env.PORT || "3000", 10);
+
+app.use(cors({
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}));
 
 app.get("/", (req: Request, res: Response): void => {
     res.send("Music Library API is live!");
